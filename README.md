1aa26f9c72f22242aeb54b2bd0673e006b183018
Hary Prihadi <phdhary@gmail.com>
2022-07-06 15:38:21 +0700

    update

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# LINUX POST INSTALL SCRIPT 
+# Fedora Linux Post-Install script
 
-This script will install apt and flatpak package listed in `apt.list` and `flatpak.list` file.
-
-Also install Flutter, SDKMAN and setup git config
-
-* Edit `apt.list` according to your needed package from aptitude
-* Edit `flatpak.list` according to your needed package from flatpak
+- What does it do?
+    * Install necessary DNF packages, SDKMAN and OMZ.
+    * Setup git config.
--- install.sh
+++ install.sh
@@ -1,57 +1,60 @@
-echo "APT DAN FLATPAK POST INSTALL"
-
-
-
 sudo -n true
-test $? -eq 0 || exit 1 "you need to be a root user"
+test $? -eq 0 || exit 1 "You need to be a root user: > sudo su"
 
-echo "===Important App==="
-echo "Updating apt repository"
-sudo apt-get update -y
-echo "Updating apt package to the latest"
-sudo apt-get upgrade -y
+echo "==Update System=="
+sudo dnf upgrade -y;
+echo "==Enabling RPM Fusion Repository=="
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm;
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm;
+sudo dnf upgrade -y;
+echo "==Docker=="
+sudo dnf remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-selinux \
+                  docker-engine-selinux \
+                  docker-engine;
+sudo dnf -y install dnf-plugins-core;
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo;
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin;
+sudo systemctl start docker;
+sudo docker run hello-world;
+echo "==Installing app listed in dnf.list=="
+cat dnf.list|xargs sudo dnf install -y;
 
-echo "Installing app listed in apt.list"
-cat apt.list|xargs sudo apt-get -y install
+echo "==NVIDIA=="
+sudo dnf install akmod-nvidia -y;
+sudo dnf install xorg-x11-drv-nvidia-cuda;
 
-#echo "Installing app listed in flatpak.list"
-#FLATPAKAPPS=$(cat flatpak.list)
-#flatpak install $FLATPAKAPPS
-
-echo "===GIT SETUP==="
-echo "Enter Git Username:"
+echo "==GIT + SSH SETUP=="
+echo "Enter Your Git Username:"
 read GITUSER;
-git config --global user.name "${GITUSER}"	
+git config --global user.name "${GITUSER}";
 
-echo "Enter Git Email:"
+echo "Enter Your Git Email:"
 read GITEMAIL;
-git config --global user.email "${GITEMAIL}"
+git config --global user.email "${GITEMAIL}";
 
-ssh-keygen -t ed25519 -C "${GITEMAIL}"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-echo "copy text below to github ssh"
-cat ~/.ssh/id_ed25519.pub
+ssh-keygen -t ed25519 -C "${GITEMAIL}";
+eval "$(ssh-agent -s)";
+ssh-add ~/.ssh/id_ed25519;
+echo "Copy text below to Github SSH settings"
+cat ~/.ssh/id_ed25519.pub;
+echo "Git is now configured!"
 
-echo "Git is now configured"
+echo "==SDKMAN=="
+curl -s "https://get.sdkman.io" | bash;
+source "$HOME/.sdkman/bin/sdkman-init.sh";
+sdk version;
+sdk install java 17.0.1-open;
 
-echo "===FLUTTER==="
-echo "Installing Flutter"
-sudo snap install flutter --classic
-flutter precache
-echo "===ANDROID STUDIO==="
-echo "Installing Android Studio"
-sudo snap install android-studio --classic
-
-
-echo "===SDKMAN==="
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
-sdk install java 17.0.1-open
-
-echo "===ZSH==="
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+echo "==ZSH=="
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
 
 EOF
